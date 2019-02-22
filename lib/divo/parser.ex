7d7bf286b84db2f29d97ba518445efe2cd109598
Timothy Regan <tregan@pillartechnology.com>
2019-02-22 15:40:01 -0500

    Private functions

--- lib/divo/parser.ex
+++ lib/divo/parser.ex
@@ -15,18 +15,18 @@ defmodule Divo.Parser do
     |> Enum.filter(&included/1)
   end
 
-  def parse_opts(configMap, opt) do
+  defp parse_opts(configMap, opt) do
     Map.get(configMap, opt, [])
     |> Enum.map(&parse_opt(&1, opt))
   end
 
-  def parse_opt({variable, value}, :env),
+  defp parse_opt({variable, value}, :env),
     do: "--env #{String.upcase(to_string(variable))}=#{value}"
 
-  def parse_opt({local, remote}, :ports), do: "-p #{local}:#{remote}"
-  def parse_opt({local, remote}, :volumes), do: "-v #{local}:#{remote}"
+  defp parse_opt({local, remote}, :ports), do: "-p #{local}:#{remote}"
+  defp parse_opt({local, remote}, :volumes), do: "-v #{local}:#{remote}"
 
-  def included(arg) do
+  defp included(arg) do
     !is_nil(arg)
   end
 end
