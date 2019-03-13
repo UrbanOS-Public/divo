b11677aed16517a89ad75dfd4d70442a2eb6a763
jeffgrunewald <jeff@grunewalddesign.com>
2019-03-12 23:35:30 -0400

    Adding top-level function delegations

--- lib/divo.ex
+++ lib/divo.ex
@@ -1,3 +1,13 @@
 defmodule Divo do
-  @moduledoc false
+  @moduledoc """
+  A library for incorporating docker-compose files or
+  compose-compliant map structures defined in application
+  config as dependency orchestration and management for
+  integration testing Elixir apps with external services
+  represented by the container services managed by divo.
+  """
+
+  defdelegate run(opts), to: Divo.Compose, as: :run
+  defdelegate stop(), to: Divo.Compose, as: :stop
+  defdelegate kill(), to: Divo.Compose, as: :kill
 end
