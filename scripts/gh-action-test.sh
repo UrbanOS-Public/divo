79ee5d61b4b95d52f4bc2a14f5e6e4ee009d8920
Christine Walsh <christine.walsh@accenture.com>
2021-07-13 23:51:18 -0400

    added github actions to divo Datastillery/divo#48

---
+++ scripts/gh-action-test.sh
@@ -0,0 +1,12 @@
+DOCKER_COMPOSE_VERSION=1.23.1
+
+sudo rm /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
+chmod +x docker-compose
+sudo mv docker-compose /usr/local/bin
+
+mix format --check-formatted
+mix credo
+mix dialyzer --halt-exit-status
+mix hex.outdated || true
+mix test
