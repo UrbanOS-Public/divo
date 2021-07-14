a6df6ba44beed30c1f78ff689b316e204d4dc4cb
Christine Walsh <christine.walsh@accenture.com>
2021-07-14 11:19:03 -0400

    remove docker compose

--- scripts/gh-action-test.sh
+++ scripts/gh-action-test.sh
@@ -1,10 +1,3 @@
-DOCKER_COMPOSE_VERSION=1.23.1
-
-sudo rm /usr/local/bin/docker-compose
-curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
-sudo mv docker-compose /usr/local/bin
-
 mix format --check-formatted
 mix credo
 mix dialyzer
