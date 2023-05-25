eeeeb84eff4cae0d744725203bd4427917183f51
nicholas.goodwin <nicholas.goodwin@accenture.com>
2023-05-25 15:37:20 -0500

    put back placebo

--- test/file_test.exs
+++ test/file_test.exs
@@ -1,20 +1,19 @@
 defmodule Divo.FileTest do
   use ExUnit.Case
-
-  import Mock
+  use Placebo
 
   require TemporaryEnv
 
   test "correctly determines the filename" do
-    with_mock(System, [], [get_env: fn ("TMPDIR") -> "/var/tmp/foo" end]) do
-      assert Divo.File.file_name() == "/var/tmp/foo/divo.compose"
-    end
+    allow(System.get_env("TMPDIR"), return: "/var/tmp/foo")
+
+    assert Divo.File.file_name() == "/var/tmp/foo/divo.compose"
   end
 
   test "correctly defaults the filename" do
-    with_mock(System, [], [get_env: fn ("TMPDIR") -> nil end]) do
-      assert Divo.File.file_name() == "/tmp/divo.compose"
-    end
+    allow(System.get_env("TMPDIR"), return: nil)
+
+    assert Divo.File.file_name() == "/tmp/divo.compose"
   end
 
   test "uses existing compose file" do
@@ -26,32 +25,32 @@ defmodule Divo.FileTest do
   end
 
   describe "generate" do
-    setup_with_mocks([
-      {System, [], [get_env: fn ("TMPDIR") -> "/var/tmp/foo" end]},
-      {File, [], [write!: fn (_, _) -> :ok end]},
-      {DivoFoobar, [:non_strict], [gen_stack: fn(_) -> %{} end]},
-      {DivoBarbaz, [:non_strict], [gen_stack: fn(_) -> %{} end]}
-    ]) do
-      {:ok, foo: "bar"}
-    end
-
     test "generates compose file from app config" do
+      allow(File.write!(any(), any()), return: :ok)
+      allow(System.get_env("TMPDIR"), return: "/var/tmp/foo")
+
       config = Divo.Helper.fetch_config()
 
       assert Divo.File.ensure_file(config) == "/var/tmp/foo/divo.compose"
     end
 
     test "generates compose file from a behaviour implementation of a single service" do
+      allow(File.write!(any(), any()), return: :ok)
+      allow(System.get_env("TMPDIR"), return: "/var/tmp/foo")
+
       services = [{DivoFoobar, [db_password: "we-are-divo", db_name: "foobar-db", something: "else"]}]
 
       TemporaryEnv.put :divo, :divo, services do
         config = Divo.Helper.fetch_config()
 
-        assert Divo.File.ensure_file(config) == "/var/tmp/foo/divo.compose"
+        assert Divo.File.ensure_file(config) == "/var/tmp/bar/divo.compose"
       end
     end
 
   test "concatenates compose file from multiple implementations of the behaviour" do
+    allow(File.write!(any(), any()), return: :ok)
+    allow(System.get_env("TMPDIR"), return: "/var/tmp/foo")
+
     services = [
       {DivoFoobar, [db_password: "we-are-divo", db_name: "foobar-db", something: "else"]},
       DivoBarbaz
