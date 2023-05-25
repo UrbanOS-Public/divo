b17af6bb33e8befbfa28f249c2d91f3348afcfe2
nicholas.goodwin <nicholas.goodwin@accenture.com>
2023-05-25 16:08:52 -0500

    fixed some tests

--- test/file_test.exs
+++ test/file_test.exs
@@ -24,43 +24,44 @@ defmodule Divo.FileTest do
     end
   end
 
-  describe "generate" do
-    test "generates compose file from app config" do
-      allow(File.write!(any(), any()), return: :ok)
-      allow(System.get_env("TMPDIR"), return: "/var/tmp/foo")
+  test "generates compose file from app config" do
+    allow(File.write!(any(), any()), return: :ok)
+    allow(System.get_env("TMPDIR"), return: "/var/tmp/foo")
 
+    config = Divo.Helper.fetch_config()
+
+    assert Divo.File.ensure_file(config) == "/var/tmp/foo/divo.compose"
+  end
+
+  test "generates compose file from a behaviour implementation of a single service" do
+    allow(File.write!(any(), any()), return: :ok)
+    allow(System.get_env("TMPDIR"), return: "/var/tmp/bar")
+    allow(DivoFoobar.gen_stack(any()), return: %{}, meck_options: [:non_strict])
+
+    services = [{DivoFoobar, [db_password: "we-are-divo", db_name: "foobar-db", something: "else"]}]
+
+    TemporaryEnv.put :divo, :divo, services do
+      config = Divo.Helper.fetch_config()
+
+      assert Divo.File.ensure_file(config) == "/var/tmp/bar/divo.compose"
+    end
+  end
+
+  test "concatenates compose file from multiple implementations of the behaviour" do
+    allow(File.write!(any(), any()), return: :ok)
+    allow(System.get_env("TMPDIR"), return: "/var/tmp/foo")
+    allow(DivoFoobar.gen_stack(any()), return: %{}, meck_options: [:non_strict])
+    allow(DivoBarbaz.gen_stack(any()), return: %{}, meck_options: [:non_strict])
+
+    services = [
+      {DivoFoobar, [db_password: "we-are-divo", db_name: "foobar-db", something: "else"]},
+      DivoBarbaz
+    ]
+
+    TemporaryEnv.put :divo, :divo, services do
       config = Divo.Helper.fetch_config()
 
       assert Divo.File.ensure_file(config) == "/var/tmp/foo/divo.compose"
-    end
-
-    test "generates compose file from a behaviour implementation of a single service" do
-      allow(File.write!(any(), any()), return: :ok)
-      allow(System.get_env("TMPDIR"), return: "/var/tmp/foo")
-
-      services = [{DivoFoobar, [db_password: "we-are-divo", db_name: "foobar-db", something: "else"]}]
-
-      TemporaryEnv.put :divo, :divo, services do
-        config = Divo.Helper.fetch_config()
-
-        assert Divo.File.ensure_file(config) == "/var/tmp/bar/divo.compose"
-      end
-    end
-
-    test "concatenates compose file from multiple implementations of the behaviour" do
-      allow(File.write!(any(), any()), return: :ok)
-      allow(System.get_env("TMPDIR"), return: "/var/tmp/foo")
-
-      services = [
-        {DivoFoobar, [db_password: "we-are-divo", db_name: "foobar-db", something: "else"]},
-        DivoBarbaz
-      ]
-
-      TemporaryEnv.put :divo, :divo, services do
-        config = Divo.Helper.fetch_config()
-
-        assert Divo.File.ensure_file(config) == "/var/tmp/foo/divo.compose"
-      end
     end
   end
 end
